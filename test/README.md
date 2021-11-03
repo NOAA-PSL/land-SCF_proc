193da6bc32590c6515a9231d5ecc94b6227254ce
ClaraDraper-NOAA <Clara.Draper@noaa.gov>
2021-11-03 19:15:11 +0000

    Updated test case with RnR input files.

--- test/README.md
+++ test/README.md
@@ -1,11 +1,10 @@
 Test case for processing the IMS observations onto UFS model grid.
 
 To run, 
-in submit_job.sh  change 1) EXEC_DIR to point to directory with your executable, set date and resolution.
 
 >submit_job.sh (can run from login node)
 
-output: IMSfSCA.20191215.C768.nc
+output: IMSfSCA.20191215.C48.nc
 
 output should match files in  ./output/ 
 
--- test/submit_job.sh
+++ test/submit_job.sh
@@ -7,14 +7,15 @@ ID=15
 IH=18
 JDAY=350
 
-RES=768
+RES=48
 
-EXEC_DIR=/scratch2/BMC/gsienkf/Clara.Draper/gerrit-hera/IMSobsproc/calcfIMS/exec/
+EXEC_DIR=../exec/
+RESTART_DIR=/scratch2/BMC/gsienkf/Clara.Draper/DA_test_cases/20191215_C${RES}/
 
 ############# NO CHANGES NEEDED BELOW THIS LINE
 export LD_LIBRARY_PATH=/apps/hdf5/1.10.5/intel/18.0.5.274/lib:/apps/nco/4.7.0/intel/18.0.3.051/lib:/apps/netcdf/4.7.4/intel/18.0.5/lib:/apps/pnetcdf/1.10.0/intel/16.1.150/impi/5.1.2.150/lib:/apps/wgrib2/2.0.8/intel/18.0.3.222/lib:/apps/intel/compilers_and_libraries_2018/linux/mpi/intel64/lib::/apps/slurm/default/lib:/apps/intel/parallel_studio_xe_2018.4.057/compilers_and_libraries_2018/linux/compiler/lib/intel64:/apps/intel/parallel_studio_xe_2018.4.057/compilers_and_libraries_2018/linux/ipp/lib/intel64:/apps/intel/parallel_studio_xe_2018.4.057/compilers_and_libraries_2018/linux/compiler/lib/intel64_lin:/apps/intel/parallel_studio_xe_2018.4.057/compilers_and_libraries_2018/linux/mkl/lib/intel64_lin:/apps/intel/parallel_studio_xe_2018.4.057/compilers_and_libraries_2018/linux/tbb/lib/intel64/gcc4.7:/apps/intel/parallel_studio_xe_2018.4.057/debugger_2018/libipt/intel64/lib:/apps/intel/parallel_studio_xe_2018.4.057/compilers_and_libraries_2018/linux/daal/lib/intel64_lin:/apps/intel/parallel_studio_xe_2018.4.057/compilers_and_libraries_2018/linux/daal/../tbb/lib/intel64_lin/gcc4.4:$LD_LIBRARY_PATH
 
-export SNOW_OBS_DIR=/scratch1/NCEPDEV/stmp2/Michael.Barlage/DA/IMSdata
+export SNOW_OBS_DIR=/scratch2/BMC/gsienkf/Clara.Draper/DA_test_cases/obs/snow/
 
 rm fims.nml
 
@@ -27,9 +28,15 @@ cat >> fims.nml << EOF
   jdate=$JDATE,
   yyyymmdd=$YYYYMMDD,
   IMS_OBS_PATH="${SNOW_OBS_DIR}/IMS/", 
-  IMS_IND_PATH="/scratch2/BMC/gsienkf/Clara.Draper/DA_test_cases/snow/IMSobsproc/get_index/"
+  IMS_IND_PATH="${SNOW_OBS_DIR}/IMS/index_files/"
   / 
 EOF
+
+# stage restarts 
+for tt in 1 2 3 4 5 6
+do 
+ln -s ${RESTART_DIR}/${YYYYMMDD}.${IH}0000.sfc_data.tile${tt}.nc . 
+done
 
 ${EXEC_DIR}/calcfIMS
 
