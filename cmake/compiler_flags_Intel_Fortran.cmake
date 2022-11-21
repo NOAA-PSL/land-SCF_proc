4938daaecf9eb0d4f5c3e75d1d3e878821a4e913
ClaraDraper-NOAA <Clara.Draper@noaa.gov>
2022-11-21 21:36:23 +0000

    removing B at start of cmake compiler script.

--- cmake/compiler_flags_Intel_Fortran.cmake
+++ cmake/compiler_flags_Intel_Fortran.cmake
@@ -1,4 +1,4 @@
-B# (C) Copyright 2018 UCAR.
+# (C) Copyright 2018 UCAR.
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
